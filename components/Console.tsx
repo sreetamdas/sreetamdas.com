c560d65504e7f779aee7eae9d2dfe1f2753be4ce
Sreetam Das <sreetamdas@gmail.com>
2020-07-18 14:13:26 +0530

    improvement: trim `<Console />`

--- components/Console.tsx
+++ components/Console.tsx
@@ -1,17 +1,10 @@
 /* eslint-disable no-console */
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 const Console = (): null => {
-	const [initialRender, setInitialRender] = useState(true);
-
 	useEffect(() => {
 		console.log("hello world!");
-		setInitialRender(false);
 	}, []);
-
-	useEffect(() => {
-		console.log("changed to:", initialRender);
-	}, [initialRender]);
 
 	return null;
 };
