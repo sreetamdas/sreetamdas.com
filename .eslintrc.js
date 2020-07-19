14855ccc3e8e46081024ecfbca32932b91a1e39c
Sreetam Das <sreetamdas@gmail.com>
2020-07-19 18:21:58 +0530

    chore: update eslint for switch

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
 	},
 	plugins: ["react", "@typescript-eslint", "react-hooks"],
 	rules: {
-		indent: ["error", "tab"],
+		indent: ["error", "tab", { SwitchCase: 1 }],
 		"linebreak-style": ["error", "unix"],
 		quotes: ["error", "double"],
 		semi: ["error", "always"],
