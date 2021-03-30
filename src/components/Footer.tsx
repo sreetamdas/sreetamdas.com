a6d6d481afe723daea2eb725d232fe966b4aed71
Sreetam Das <sreetamdas@gmail.com>
2021-03-31 02:51:59 +0530

    chore: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,9 @@ import styled from "styled-components";
 const Footer = () => {
 	return (
 		<FooterContent>
-			Made with <a href="https://nextjs.org">Next.js</a> &bull;{" "}
-			<a href="https://github.com/sreetamdas/sreetamdas.com">
-				View source on Github
-			</a>{" "}
+			Made with <a href="https://nextjs.org">Next.js</a> &bull; Hosted on{" "}
+			<a href="https://netlify.com">Netlify</a> &bull; View source on{" "}
+			<a href="https://github.com/sreetamdas/sreetamdas.com">Github</a>{" "}
 			&bull; Find me on{" "}
 			<a href="https://twitter.com/_SreetamDas">Twitter</a>
 		</FooterContent>
