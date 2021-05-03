f367b8747011591b6b66bf25fddeac11d36e8f92
Sreetam Das <sreetamdas@gmail.com>
2021-05-03 12:54:18 +0530

    chore: remove fancy pants elements from greenscreen (???)

--- src/pages/talks/greenscreen.tsx
+++ src/pages/talks/greenscreen.tsx
@@ -4,35 +4,9 @@ import { ThemeContext } from "styled-components";
 import { GreenScreen } from "components/talks/greenscreen";
 import { FullWidth } from "styles/layouts";
 import { Title } from "styles/typography";
-import { random, useInterval, useTimeout } from "utils/hooks";
 
 const Page = () => {
-	let root: HTMLElement;
 	const { changeThemeVariant } = useContext(ThemeContext);
-
-	const getNewColor = () => {
-		const h = random(1, 360);
-		const s = random(80, 90);
-		const l = random(50, 60);
-
-		return `hsl(${h}, ${s}%, ${l}%)`;
-	};
-
-	const changeColor = () => {
-		const newColor = getNewColor();
-
-		if (root === undefined) root = document?.documentElement;
-		root.style.setProperty("--color-fancy-pants", newColor);
-	};
-
-	useInterval(() => {
-		changeColor();
-	}, 5000);
-
-	// HACK: begin the color transition, adding it in useMount doesn't set off the CSS transition
-	useTimeout(() => {
-		changeColor();
-	}, 0);
 
 	useEffect(() => {
 		changeThemeVariant("dark");
