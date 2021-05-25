3ceb3edad288a6cb2c5f863ad46a2f57474e663a
Sreetam Das <sreetamdas@gmail.com>
2021-05-25 12:10:36 +0530

    improvement: playground SEO

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,11 +1,14 @@
 import { Fragment } from "react";
 
 import { LiveCode } from "components/MDXLiveCode";
+import { DocumentHead } from "components/shared/seo";
 import { Title } from "styles/typography";
 
 const Playground = () => {
 	return (
 		<Fragment>
+			<DocumentHead title="Playground 🥳" />
+
 			<Title size={5}>
 				Playground{" "}
 				<span role="img" aria-label="partying-face">
