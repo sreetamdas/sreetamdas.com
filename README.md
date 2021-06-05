ed06ffcffe4b56817a72a06e9b18b12939e3ebb2
Sreetam Das <sreetamdas@gmail.com>
2021-06-05 17:09:39 +0530

    chore: wrap footer content on mobile, upgrade deps, update README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [sreetamdas.com](https://sreetamdas.com)
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/fea4626e-2a86-4625-a20c-6cb1d86ad8e2/deploy-status)](https://app.netlify.com/sites/sreetamdas/deploys)
+
 <img src="public/lighthouse_scores.png" align="center" alt="Lighthouse scores' screenshot: 100, 100, 100, 100" />
 
 <h5 align="center">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+import { breakpoint } from "utils/style";
 
 export const Footer = () => {
 	return (
 		<FooterContent>
 			Made with <a href="https://nextjs.org">Next.js</a> &bull; Hosted on{" "}
-			<a href="https://netlify.com">Netlify</a> &bull; View source on{" "}
-			<a href="https://github.com/sreetamdas/sreetamdas.com">Github</a> &bull; Find me on{" "}
-			<a href="https://twitter.com/_SreetamDas">Twitter</a>
+			<a href="https://netlify.com">Netlify</a> <span>&bull;</span> <br />
+			View source on <a href="https://github.com/sreetamdas/sreetamdas.com">Github</a> &bull; Find
+			me on <a href="https://twitter.com/_SreetamDas">Twitter</a>
 		</FooterContent>
 	);
 };
@@ -15,4 +17,16 @@ const FooterContent = styled.div`
 	font-size: 0.8rem;
 	padding: 0 1rem;
 	text-align: center;
+
+	& > br {
+		${breakpoint.from.md(css`
+			display: none;
+		`)}
+	}
+
+	& > span {
+		${breakpoint.until.md(css`
+			display: none;
+		`)}
+	}
 `;
