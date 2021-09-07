4cc02a43b9e9e73acc02db304b497f2873634d3b
Sreetam Das <sreetamdas@gmail.com>
2021-09-07 22:31:13 +0530

    feat: add API route `/api/page/get-views` to read page views for specified slug

---
+++ src/pages/api/page/get-views.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { PostDetails } from "typings/blog";
+import { supabase } from "utils/supabaseClient";
+
+/**
+ * @api {post} /api/page/add-view Add view to page using Supabase client
+ */
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method === "GET") {
+		const { page_slug } = req.body;
+
+		const { data, error } = await supabase
+			.from<PostDetails>("page_details")
+			.select("view_count")
+			.eq("slug", page_slug)
+			.limit(1)
+			.single();
+
+		if (error) {
+			res.status(200).send({ views: 0 });
+		} else {
+			const { view_count: views } = data ?? { view_count: 0 };
+			res.status(200).send({ views });
+		}
+	} else {
+		res.status(400).send("Bad request");
+	}
+};
