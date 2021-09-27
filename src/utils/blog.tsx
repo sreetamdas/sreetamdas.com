661d3add198a4c19afa1c9338e7629760bb036a0
Sreetam Das <sreetamdas@gmail.com>
2021-09-28 01:19:58 +0530

    chore: add `ESBUILD_BINARY_PATH` only for Netlify

--- src/utils/blog.tsx
+++ src/utils/blog.tsx
@@ -6,21 +6,23 @@ import remarkSlug from "remark-slug";
 
 import { TBlogPostPageProps } from "typings/blog";
 
-if (process.platform === "win32") {
-	process.env.ESBUILD_BINARY_PATH = path.join(
-		process.cwd(),
-		"node_modules",
-		"esbuild",
-		"esbuild.exe"
-	);
-} else {
-	process.env.ESBUILD_BINARY_PATH = path.join(
-		process.cwd(),
-		"node_modules",
-		"esbuild",
-		"bin",
-		"esbuild"
-	);
+if (process.env.NETLIFY && !process.env.ESBUILD_BINARY_PATH) {
+	if (process.platform === "win32") {
+		process.env.ESBUILD_BINARY_PATH = path.join(
+			process.cwd(),
+			"node_modules",
+			"esbuild",
+			"esbuild.exe"
+		);
+	} else {
+		process.env.ESBUILD_BINARY_PATH = path.join(
+			process.cwd(),
+			"node_modules",
+			"esbuild",
+			"bin",
+			"esbuild"
+		);
+	}
 }
 
 const PATH = path.resolve(process.cwd(), "src");
