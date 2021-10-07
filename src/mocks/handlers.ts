bb460c12e7a03449631aed7d02206f49bf7be43b
Sreetam Das <sreetamdas@gmail.com>
2021-10-07 12:10:08 +0530

    refactor: global styles and mock handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,33 +1,27 @@
 import { rest } from "msw";
 
 export const handlers = [
-	rest.post("/api/coffee", (_req, res, ctx) => {
-		return res(ctx.status(200), ctx.json({ message: "I'm NOT a teapot" }));
-	}),
-	rest.post("https://telemetry.nextjs.org/api/v1/record", (_req, res, ctx) => {
-		return res(ctx.status(200), ctx.json({ message: "I'm NOT a teapot" }));
-	}),
-
+	// Buttondown.email
 	rest.get("https://api.buttondown.email/v1/subscribers", (_req, res, ctx) => {
 		return res(ctx.status(200), ctx.json({ count: 69 }));
 	}),
 
+	// GitHub stats
 	rest.get("https://api.github.com/repos/sreetamdas/sreetamdas.com", (_req, res, ctx) => {
-		return res(ctx.status(200), ctx.json({ stargazers_count: 69, forks: 69 }));
+		return res(ctx.status(200));
+	}),
+	rest.post("/api/github/stats", (_req, res, ctx) => {
+		return res(ctx.status(200), ctx.json({ stars: 69, forks: 69 }));
 	}),
 
+	// Supabase
 	rest.post(
 		"https://shpiudmeykaahmmhipos.supabase.co/rest/v1/rpc/upsert_page_view",
 		(_req, res, ctx) => {
-			return res(ctx.status(200), ctx.body("69"));
+			return res(ctx.status(200));
 		}
 	),
-
-	rest.post("/api/github/stats", (_req, res, ctx) => {
-		return res(ctx.status(200), ctx.json({ stars: 96, forks: 96 }));
-	}),
-
 	rest.post("/api/page/*", (_req, res, ctx) => {
-		return res(ctx.status(200), ctx.json({ view_count: 96 }));
+		return res(ctx.status(200), ctx.json({ view_count: 69 }));
 	}),
 ];
