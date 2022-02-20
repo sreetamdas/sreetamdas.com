d66e24ab4a87a578e6d7a18cbf6223e216afd6b1
Sreetam Das <sreetamdas@gmail.com>
2022-02-20 23:12:50 +0530

    chore: rename supabase client export

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
