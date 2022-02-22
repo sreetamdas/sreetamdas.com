0ea68b023df3345bc7a23f522034c8867e456003
Sreetam Das <sreetamdas@gmail.com>
2022-02-22 12:13:18 +0530

    improvement: admin page works with GitHub OAuth
    Supabase Auth 🚀

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,79 @@
+import { Session } from "@supabase/supabase-js";
+import { useState, useEffect } from "react";
+
+import { ViewsCounter } from "@/components/ViewsCounter";
+import { DocumentHead } from "@/components/shared/seo";
+import { Center, Space } from "@/styles/layouts";
+import { Title } from "@/styles/typography";
+import { supabaseClient } from "@/utils/supabaseClient";
+import { useHasMounted } from "@/utils/hooks";
+import { UploadBook } from "@/components/Admin/uploadBook";
+
+const AdminLogin = () => {
+	async function handleSignUpWIthGitHub() {
+		await supabaseClient.auth.signIn(
+			{
+				provider: "github",
+			},
+			{ redirectTo: "http://localhost:3000/admin" }
+		);
+	}
+
+	return (
+		<>
+			<DocumentHead title="/admin" noIndex />
+
+			<Center>
+				<Title size={5}>/admin</Title>
+				<Space />
+
+				<button onClick={handleSignUpWIthGitHub}>Sign in with GitHub</button>
+			</Center>
+			<ViewsCounter />
+		</>
+	);
+};
+
+const Admin = () => {
+	const hasMounted = useHasMounted();
+	const [session, setSession] = useState<Session | null>(supabaseClient.auth.session());
+
+	async function handleSignOut() {
+		await supabaseClient.auth.signOut();
+	}
+
+	useEffect(() => {
+		setSession(supabaseClient.auth.session());
+
+		supabaseClient.auth.onAuthStateChange((_event, session) => {
+			setSession(session);
+		});
+	}, []);
+
+	if (!hasMounted) {
+		return null;
+	}
+	if (!session) {
+		return <AdminLogin />;
+	}
+	return (
+		<>
+			<DocumentHead title="/admin" noIndex />
+
+			<Center>
+				<Title size={5}>/admin</Title>
+				<Title size={2}>Signed in successfully.</Title>
+
+				<span>
+					{/* Signed in as {user.email} */}
+					<button onClick={handleSignOut}>Sign out</button>
+				</span>
+
+				<UploadBook />
+			</Center>
+			<ViewsCounter />
+		</>
+	);
+};
+
+export default Admin;
