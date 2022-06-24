777011969da5184318cab7acb5f10176a63dd773
Sreetam Das <sreetamdas@gmail.com>
2022-06-25 01:07:40 +0530

    chore: fix types for styled-components in `_document`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,15 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from "next/document";
+import Document, {
+	DocumentContext,
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentInitialProps,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
 		const sheet = new ServerStyleSheet();
 		const originalRenderPage = ctx.renderPage;
 
@@ -15,12 +22,7 @@ export default class MyDocument extends Document {
 			const initialProps = await Document.getInitialProps(ctx);
 			return {
 				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
+				styles: [initialProps.styles, sheet.getStyleElement()],
 			};
 		} finally {
 			sheet.seal();
