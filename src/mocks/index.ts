fef79ff7dc144ea34cc0835350e65959eddd4593
Sreetam Das <sreetamdas@gmail.com>
2022-06-29 14:03:20 +0530

    improvement: ignore Sentry API calls while mocking using MSW

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,10 +1,36 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+import { SetupWorkerApi, SharedOptions } from "msw";
+
+import { SENTRY_ENVELOPE_URL } from "@/domains/sentry";
+
+type UnhandledRequestStrategy = NonNullable<SharedOptions["onUnhandledRequest"]>;
+type UnhandledRequestCallback = Parameters<
+	Exclude<UnhandledRequestStrategy, "bypass" | "warn" | "error">
+>;
+
+function onUnhandledRequest(
+	request: UnhandledRequestCallback[0],
+	print: UnhandledRequestCallback[1]
+) {
+	// Ignore unhandled Sentry requests.
+	if (request.url.href.startsWith(SENTRY_ENVELOPE_URL)) {
+		return;
+	}
+
+	// Otherwise, execute the default warning/error/ strategy.
+	print.warning(); // or "print.error()"
+}
+
 if (typeof window === "undefined") {
 	const { server } = require("./server");
-	server.listen();
+	server.listen({
+		onUnhandledRequest,
+	});
 } else {
-	const { worker } = require("./browser");
-	worker.start();
+	const { worker } = require("./browser") as { worker: SetupWorkerApi };
+	worker.start({
+		onUnhandledRequest,
+	});
 }
 
 export {};
