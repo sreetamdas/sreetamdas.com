d0d3cc82639f39e358d294171a87ff295e055b8f
Sreetam Das <sreetamdas@gmail.com>
2022-06-29 14:22:47 +0530

    improvement(msw): handle missing Sentry DSN upon unhandled requests

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -13,6 +13,7 @@ function onUnhandledRequest(
 	print: UnhandledRequestCallback[1]
 ) {
 	// Ignore unhandled Sentry requests.
+	// @ts-expect-error we're not adding this handler when SENTRY_ENVELOPE_URL is undefined
 	if (request.url.href.startsWith(SENTRY_ENVELOPE_URL)) {
 		return;
 	}
@@ -21,16 +22,28 @@ function onUnhandledRequest(
 	print.warning(); // or "print.error()"
 }
 
+let workerOptions;
 if (typeof window === "undefined") {
+	if (typeof SENTRY_ENVELOPE_URL !== "undefined") {
+		workerOptions = {
+			onUnhandledRequest,
+		};
+	}
 	const { server } = require("./server");
-	server.listen({
-		onUnhandledRequest,
-	});
+	server.listen(workerOptions);
 } else {
+	if (typeof SENTRY_ENVELOPE_URL !== "undefined") {
+		// eslint-disable-next-line no-console
+		console.info("[MSW] Info: Sentry DSN found. Skipping logging unhandled Sentry API requests.");
+		workerOptions = {
+			onUnhandledRequest,
+		};
+	} else {
+		// eslint-disable-next-line no-console
+		console.warn("[MSW] Warning: Sentry DSN is missing. Logging unhandled Sentry API requests.");
+	}
 	const { worker } = require("./browser") as { worker: SetupWorkerApi };
-	worker.start({
-		onUnhandledRequest,
-	});
+	worker.start(workerOptions);
 }
 
 export {};
