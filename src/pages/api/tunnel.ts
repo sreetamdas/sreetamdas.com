887d8ff96da0554755129170fa3b20fd03870dcc
Sreetam Das <sreetamdas@gmail.com>
2022-07-20 02:16:59 +0530

    fix: update Sentry tunnel

--- src/pages/api/tunnel.ts
+++ src/pages/api/tunnel.ts
@@ -3,31 +3,31 @@ import * as url from "url";
 import { withSentry, captureException } from "@sentry/nextjs";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// Change host appropriately if you run your own Sentry instance.
-const sentryHost = "sentry.io";
+import { getSentryKeyElements } from "@/domains/sentry";
 
 // Set knownProjectIds to an array with your Sentry project IDs which you
 // want to accept through this proxy.
-// const knownProjectIds = [];
+const { ingestURL, projectID } = getSentryKeyElements();
+const knownProjectIds = [projectID];
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
 	try {
 		const envelope = req.body;
 		const pieces = envelope.split("\n");
-
 		const header = JSON.parse(pieces[0]);
 
 		const { host, path } = url.parse(header.dsn);
-		if (host !== sentryHost) {
+
+		if (host !== ingestURL) {
 			throw new Error(`invalid host: ${host}`);
 		}
 
-		const projectId = path?.endsWith("/") ? path.slice(0, -1) : path;
-		// if (!knownProjectIds.includes(projectId)) {
-		// 	throw new Error(`invalid project id: ${projectId}`);
-		// }
+		const projectId = path?.replace(/\//g, "");
+		if (!knownProjectIds.includes(projectId)) {
+			throw new Error(`invalid project id: ${projectId}`);
+		}
 
-		const sentryUrl = `https://${sentryHost}/api/${projectId}/envelope/`;
+		const sentryUrl = `https://${ingestURL}/api/${projectId}/envelope/`;
 		const response = await fetch(sentryUrl, {
 			method: "POST",
 			body: envelope,
