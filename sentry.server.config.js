bf9af93078385838266b3da1600a961a79aec92a
Sreetam Das <sreetamdas@gmail.com>
2022-07-22 14:25:06 +0530

    chore: disable Sentry in development

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,16 +7,20 @@ import { BrowserTracing } from "@sentry/tracing";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 const ENVIRONMENT = process.env.VERCEL_ENV || process.env.NODE_ENV;
-Sentry.init({
-	dsn: SENTRY_DSN,
-	// This enables automatic instrumentation (highly recommended), but is not
-	// necessary for purely manual usage
-	integrations: [new BrowserTracing()],
-	// Adjust this value in production, or use tracesSampler for greater control
-	tracesSampleRate: 0.25,
-	environment: ENVIRONMENT,
-	// ...
-	// Note: if you want to override the automatic release value, do not set a
-	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
-	// that it will also get attached to your source maps
-});
+
+// Disable sentry reporting in development
+if (ENVIRONMENT !== "development") {
+	Sentry.init({
+		dsn: SENTRY_DSN,
+		// This enables automatic instrumentation (highly recommended), but is not
+		// necessary for purely manual usage
+		integrations: [new BrowserTracing()],
+		// Adjust this value in production, or use tracesSampler for greater control
+		tracesSampleRate: 0.25,
+		environment: ENVIRONMENT,
+		// ...
+		// Note: if you want to override the automatic release value, do not set a
+		// `release` value here - use the environment variable `SENTRY_RELEASE`, so
+		// that it will also get attached to your source maps
+	});
+}
