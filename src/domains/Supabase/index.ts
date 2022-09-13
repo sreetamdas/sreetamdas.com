b69563c48d619a3b873271c68aafb3097f5c7472
Sreetam Das <sreetamdas@gmail.com>
2022-09-13 22:51:21 +0530

    chore: throw error when Supabase env vars are missing

--- src/domains/Supabase/index.ts
+++ src/domains/Supabase/index.ts
@@ -5,6 +5,12 @@ import { randomAlphanumeric } from "@/utils/hooks";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
+if (supabaseUrl === "" || supabaseAnonKey === "") {
+	throw new Error("This project needs Supabase to be set-up in order to run properly.", {
+		cause: "Missing env variables Add NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY",
+	});
+}
+
 export const SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
 export function getSupabaseFileURL(filename: string) {
@@ -12,6 +18,7 @@ export function getSupabaseFileURL(filename: string) {
 }
 
 type UploadFileToSupabaseProps = { bucket?: string; path?: string; useExactFilename?: boolean };
+
 export async function uploadFileToSupabase(
 	image: File,
 	{ bucket = "public", path = "/", useExactFilename = false }: UploadFileToSupabaseProps
