04cc19b5e1e1df3ecabb0ffc3984bb17441aad7b
Sreetam Das <sreetamdas@gmail.com>
2022-10-13 12:53:15 +0530

    chore: remove isAdmin text from Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,25 +1,19 @@
-import { useSessionContext } from "@supabase/auth-helpers-react";
 import styled, { css } from "styled-components";
 
 import { GitHubStats } from "@/components/Footer/GitHubStats";
 import { Foobar } from "@/components/foobar";
 import { breakpoint } from "@/utils/style";
 
-export const Footer = () => {
-	const { session } = useSessionContext();
-
-	return (
-		<FooterWrapper>
-			{session && "isAdmin"}
-			<Foobar />
-			<GitHubStats />
-			Made with <a href="https://nextjs.org">Next.js</a> &bull; View source on{" "}
-			<a href="https://github.com/sreetamdas/sreetamdas.com">Github</a>
-			<span>&bull;</span> <br />
-			Find me on <a href="https://twitter.com/_SreetamDas">Twitter</a>
-		</FooterWrapper>
-	);
-};
+export const Footer = () => (
+	<FooterWrapper>
+		<Foobar />
+		<GitHubStats />
+		Made with <a href="https://nextjs.org">Next.js</a> &bull; View source on{" "}
+		<a href="https://github.com/sreetamdas/sreetamdas.com">Github</a>
+		<span>&bull;</span> <br />
+		Find me on <a href="https://twitter.com/_SreetamDas">Twitter</a>
+	</FooterWrapper>
+);
 
 const FooterWrapper = styled.footer`
 	position: sticky;
