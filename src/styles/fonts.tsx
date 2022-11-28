e3b9f7a8b0e4a6e5a1b140ce5f49855c795950ff
Sreetam Das <sreetamdas@gmail.com>
2022-11-28 12:09:06 +0530

    improvement: use Iosevka (Das' version) custom build

--- src/styles/fonts.tsx
+++ src/styles/fonts.tsx
@@ -9,12 +9,12 @@ export const iosevkaFont = localFont({
 	variable: "--font-iosevka",
 	src: [
 		{
-			path: "../../public/fonts/iosevka/woff2/iosevka-regular.woff2",
+			path: "../../public/fonts/iosevka/iosevka-das-version-regular.woff2",
 			weight: "400",
 			style: "normal",
 		},
 		{
-			path: "../../public/fonts/iosevka/woff2/iosevka-italic.woff2",
+			path: "../../public/fonts/iosevka/iosevka-das-version-italic.woff2",
 			weight: "400",
 			style: "italic",
 		},
