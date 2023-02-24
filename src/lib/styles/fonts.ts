9f786ebab5ff5327b3aea767e386b38b8ccc0bd6
Sreetam Das <sreetamdas@gmail.com>
2023-02-24 10:56:15 +0530

    chore: run codemod for `@next/font`, use built-in `next/font` instead

--- src/lib/styles/fonts.ts
+++ src/lib/styles/fonts.ts
@@ -1,5 +1,5 @@
-import { Inter } from "@next/font/google";
-import localFont from "@next/font/local";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
 export const interFont = Inter({
 	variable: "--font-inter",
