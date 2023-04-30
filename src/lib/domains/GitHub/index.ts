063a5b7121f86bc3fd164f476792f1176e23a219
Sreetam Das <sreetamdas@gmail.com>
2023-04-30 10:36:52 +0530

    improvement: add error handling for `getGitHubStats()`

--- src/lib/domains/GitHub/index.ts
+++ src/lib/domains/GitHub/index.ts
@@ -1,4 +1,5 @@
 import { Octokit } from "@octokit/core";
+import { captureException } from "@sentry/nextjs";
 
 import { DEFAULT_REPO } from "@/config";
 
@@ -8,9 +9,15 @@ export const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
  * Fetch GitHub stats
  */
 export async function getGitHubStats() {
-	const {
-		data: { stargazers_count: stars = 0, forks = 0 },
-	} = await octokit.request("GET /repos/{owner}/{repo}", DEFAULT_REPO);
+	try {
+		const {
+			data: { stargazers_count: stars, forks },
+		} = await octokit.request("GET /repos/{owner}/{repo}", DEFAULT_REPO);
 
-	return { stars, forks };
+		return { stars, forks };
+	} catch (error) {
+		captureException(error);
+
+		return { stars: 0, forks: 0 };
+	}
 }
