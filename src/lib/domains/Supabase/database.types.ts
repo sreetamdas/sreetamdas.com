602db5c307603d7d293fa314641615f1ecbca717
Sreetam Das <sreetamdas@gmail.com>
2023-05-04 14:43:20 +0530

    chore: update types for Supabase database

--- src/lib/domains/Supabase/database.types.ts
+++ src/lib/domains/Supabase/database.types.ts
@@ -1,92 +1,57 @@
 export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
+	graphql_public: {
+		Tables: {
+			[_ in never]: never;
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			graphql: {
+				Args: {
+					operationName?: string;
+					query?: string;
+					variables?: Json;
+					extensions?: Json;
+				};
+				Returns: Json;
+			};
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
 	public: {
 		Tables: {
-			authors: {
-				Row: {
-					id: number;
-					created_at: string | null;
-					name: string;
-				};
-				Insert: {
-					id?: number;
-					created_at?: string | null;
-					name: string;
-				};
-				Update: {
-					id?: number;
-					created_at?: string | null;
-					name?: string;
-				};
-			};
-			book_status: {
-				Row: {
-					id: number;
-					created_at: string | null;
-					value: string;
-				};
-				Insert: {
-					id?: number;
-					created_at?: string | null;
-					value: string;
-				};
-				Update: {
-					id?: number;
-					created_at?: string | null;
-					value?: string;
-				};
-			};
-			books: {
-				Row: {
-					id: number;
-					created_at: string | null;
-					name: string;
-					cover: string;
-					status: string;
-					author: string;
-				};
-				Insert: {
-					id?: number;
-					created_at?: string | null;
-					name: string;
-					cover: string;
-					status: string;
-					author: string;
-				};
-				Update: {
-					id?: number;
-					created_at?: string | null;
-					name?: string;
-					cover?: string;
-					status?: string;
-					author?: string;
-				};
-			};
 			page_details: {
 				Row: {
-					id: number;
-					slug: string;
 					created_at: string;
+					id: number;
+					likes: number;
+					slug: string;
 					updated_at: string;
 					view_count: number;
-					likes: number;
 				};
 				Insert: {
-					id?: number;
-					slug: string;
 					created_at?: string;
+					id?: number;
+					likes?: number;
+					slug: string;
 					updated_at?: string;
 					view_count?: number;
-					likes?: number;
 				};
 				Update: {
-					id?: number;
-					slug?: string;
 					created_at?: string;
+					id?: number;
+					likes?: number;
+					slug?: string;
 					updated_at?: string;
 					view_count?: number;
-					likes?: number;
 				};
 			};
 		};
@@ -95,11 +60,178 @@ export interface Database {
 		};
 		Functions: {
 			upsert_page_view: {
-				Args: { page_slug: string };
+				Args: {
+					page_slug: string;
+				};
 				Returns: number;
 			};
 		};
 		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
+	storage: {
+		Tables: {
+			buckets: {
+				Row: {
+					allowed_mime_types: string[] | null;
+					avif_autodetection: boolean | null;
+					created_at: string | null;
+					file_size_limit: number | null;
+					id: string;
+					name: string;
+					owner: string | null;
+					public: boolean | null;
+					updated_at: string | null;
+				};
+				Insert: {
+					allowed_mime_types?: string[] | null;
+					avif_autodetection?: boolean | null;
+					created_at?: string | null;
+					file_size_limit?: number | null;
+					id: string;
+					name: string;
+					owner?: string | null;
+					public?: boolean | null;
+					updated_at?: string | null;
+				};
+				Update: {
+					allowed_mime_types?: string[] | null;
+					avif_autodetection?: boolean | null;
+					created_at?: string | null;
+					file_size_limit?: number | null;
+					id?: string;
+					name?: string;
+					owner?: string | null;
+					public?: boolean | null;
+					updated_at?: string | null;
+				};
+			};
+			migrations: {
+				Row: {
+					executed_at: string | null;
+					hash: string;
+					id: number;
+					name: string;
+				};
+				Insert: {
+					executed_at?: string | null;
+					hash: string;
+					id: number;
+					name: string;
+				};
+				Update: {
+					executed_at?: string | null;
+					hash?: string;
+					id?: number;
+					name?: string;
+				};
+			};
+			objects: {
+				Row: {
+					bucket_id: string | null;
+					created_at: string | null;
+					id: string;
+					last_accessed_at: string | null;
+					metadata: Json | null;
+					name: string | null;
+					owner: string | null;
+					path_tokens: string[] | null;
+					updated_at: string | null;
+					version: string | null;
+				};
+				Insert: {
+					bucket_id?: string | null;
+					created_at?: string | null;
+					id?: string;
+					last_accessed_at?: string | null;
+					metadata?: Json | null;
+					name?: string | null;
+					owner?: string | null;
+					path_tokens?: string[] | null;
+					updated_at?: string | null;
+					version?: string | null;
+				};
+				Update: {
+					bucket_id?: string | null;
+					created_at?: string | null;
+					id?: string;
+					last_accessed_at?: string | null;
+					metadata?: Json | null;
+					name?: string | null;
+					owner?: string | null;
+					path_tokens?: string[] | null;
+					updated_at?: string | null;
+					version?: string | null;
+				};
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			can_insert_object: {
+				Args: {
+					bucketid: string;
+					name: string;
+					owner: string;
+					metadata: Json;
+				};
+				Returns: undefined;
+			};
+			extension: {
+				Args: {
+					name: string;
+				};
+				Returns: string;
+			};
+			filename: {
+				Args: {
+					name: string;
+				};
+				Returns: string;
+			};
+			foldername: {
+				Args: {
+					name: string;
+				};
+				Returns: unknown;
+			};
+			get_size_by_bucket: {
+				Args: Record<PropertyKey, never>;
+				Returns: {
+					size: number;
+					bucket_id: string;
+				}[];
+			};
+			search: {
+				Args: {
+					prefix: string;
+					bucketname: string;
+					limits?: number;
+					levels?: number;
+					offsets?: number;
+					search?: string;
+					sortcolumn?: string;
+					sortorder?: string;
+				};
+				Returns: {
+					name: string;
+					id: string;
+					updated_at: string;
+					created_at: string;
+					last_accessed_at: string;
+					metadata: Json;
+				}[];
+			};
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
 			[_ in never]: never;
 		};
 	};
