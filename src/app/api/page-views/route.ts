364570f0cafb6cf3d4eed4312a279e04cf314bdf
Sreetam Das <sreetamdas@gmail.com>
2023-05-06 20:35:55 +0530

    refactor: Supabase client

--- src/app/api/page-views/route.ts
+++ src/app/api/page-views/route.ts
@@ -1,52 +1,46 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-import { getSupabaseClient } from "@/lib/domains/Supabase";
+import { supabaseClient } from "@/lib/domains/Supabase/client";
 
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-	const { enabled: supabaseEnabled, supabaseClient } = getSupabaseClient();
+	const { searchParams } = new URL(request.url);
+	const slug = searchParams.get("slug");
 
-	if (!supabaseEnabled) {
-		return NextResponse.json({ error: "Supabase has not been intialised" }, { status: 500 });
+	if (slug === null) {
+		return NextResponse.json(
+			{ error: 'Request is missing required "slug" param' },
+			{ status: 400 }
+		);
 	} else {
-		const { searchParams } = new URL(request.url);
-		const slug = searchParams.get("slug");
+		const { data, error } = await supabaseClient
+			.from("page_details")
+			.select("view_count")
+			.eq("slug", slug)
+			.limit(1)
+			.single();
 
-		if (slug === null) {
-			return NextResponse.json(
-				{ error: 'Request is missing required "slug" param' },
-				{ status: 400 }
-			);
-		} else {
-			const { data, error } = await supabaseClient
-				.from("page_details")
-				.select("view_count")
-				.eq("slug", slug)
-				.limit(1)
-				.single();
-
-			if (error) {
-				if (error.code === "PGRST116") {
-					// TODO add exception handler
-					// captureException(error);
-					return NextResponse.json(
-						{
-							view_count: 0,
-							message: `Page '${slug}' has not been added to Supabase yet`,
-						},
-						{ status: 200 }
-					);
-				} else {
-					// TODO add exception handler
-					// captureException(error);
-					return NextResponse.json({ error }, { status: 500 });
-				}
+		if (error) {
+			if (error.code === "PGRST116") {
+				// TODO add exception handler
+				// captureException(error);
+				return NextResponse.json(
+					{
+						view_count: 0,
+						message: `Page '${slug}' has not been added to Supabase yet`,
+					},
+					{ status: 200 }
+				);
 			} else {
-				const { view_count } = data;
-				return NextResponse.json({ view_count }, { status: 200 });
+				// TODO add exception handler
+				// captureException(error);
+				return NextResponse.json({ error }, { status: 500 });
 			}
+		} else {
+			const { view_count } = data;
+			return NextResponse.json({ view_count }, { status: 200 });
 		}
 	}
 }
--- src/lib/domains/Supabase/index.ts
+++ src/lib/domains/Supabase/index.ts
@@ -1,51 +1,6 @@
 import { captureException } from "@sentry/nextjs";
-import type { SupabaseClient } from "@supabase/supabase-js";
-import { createClient } from "@supabase/supabase-js";
 
-import type { Database } from "./database.types";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
-
-if (supabaseUrl === "" || supabaseAnonKey === "") {
-	// TODO Fix error when Supabase is not initialized
-	// throw new Error("This project needs Supabase to be set-up in order to run properly.", {
-	// 	cause: "Missing env variables Add NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY",
-	// });
-}
-
-export const supabaseEnabled =
-	typeof process.env.NEXT_PUBLIC_SUPABASE_URL !== "undefined" &&
-	process.env.NEXT_PUBLIC_SUPABASE_URL !== "" &&
-	typeof process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY !== "undefined" &&
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY !== "";
-
-const supabaseClient = supabaseEnabled
-	? createClient<Database>(supabaseUrl, supabaseAnonKey)
-	: undefined;
-
-type SupabaseClientUnknown =
-	| {
-			enabled: true;
-			supabaseClient: SupabaseClient<Database>;
-	  }
-	| {
-			enabled: false;
-			supabaseClient: undefined;
-	  };
-
-export function getSupabaseClient(): SupabaseClientUnknown {
-	if (supabaseEnabled && typeof supabaseClient !== "undefined") {
-		return {
-			enabled: true,
-			supabaseClient: supabaseClient,
-		};
-	}
-	return {
-		enabled: false,
-		supabaseClient: undefined,
-	};
-}
+import { supabaseEnabled } from "./client";
 
 type PageViewCount = {
 	view_count: number;
