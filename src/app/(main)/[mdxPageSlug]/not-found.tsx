65d8306539775c8fec43b8ebe29e897625b54f44
Sreetam Das <sreetamdas@gmail.com>
2023-06-24 22:42:17 +0530

    wip: add `[mdxPageSlug]` pages

---
+++ src/app/(main)/[mdxPageSlug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NewsletterEmailNotFound() {
+	return (
+		<>
+			<h1>Not Found</h1>
+			<p>The newsletter issue/email you&apos;re trying to find doesn&apos;t exist :/</p>
+		</>
+	);
+}
--- src/app/(main)/[mdxPageSlug]/page.tsx
+++ src/app/(main)/[mdxPageSlug]/page.tsx
@@ -1,40 +1,32 @@
-import { promises as fs } from "fs";
-import path from "path";
+"use client";
 
-import { Suspense } from "react";
+import dynamic from "next/dynamic";
+// import { Suspense } from "react";
 
-import type { MDXPagePaths } from "./MDXPage";
-import { getComponentFromSlug } from "./MDXPage";
+// export const dynamicParams = false;
 
-export const dynamicParams = false;
+// export async function generateStaticParams() {
+// 	const MDX_PAGES_DIR = path.resolve(process.cwd(), "src", "app", "(main)", "[mdxPageSlug]");
+// 	const files = (await fs.readdir(MDX_PAGES_DIR)).filter((file) => file.endsWith(".mdx"));
 
-export async function generateStaticParams() {
-	const MDX_PAGES_DIR = path.resolve(process.cwd(), "src", "app", "(main)", "[mdxPageSlug]");
-	const files = (await fs.readdir(MDX_PAGES_DIR)).filter((file) => file.endsWith(".mdx"));
+// 	const params = files.map((fileName) => {
+// 		const pageName = fileName.replace(/\.mdx?$/, "");
+// 		return {
+// 			mdxPageSlug: pageName,
+// 		};
+// 	});
 
-	const params = files.map((fileName) => {
-		const pageName = fileName.replace(/\.mdx?$/, "");
-		return {
-			mdxPageSlug: pageName,
-		};
-	});
-
-	return params;
-}
+// 	return params;
+// }
 
 type PageParamsType = {
 	params: {
-		mdxPageSlug: MDXPagePaths;
+		mdxPageSlug: string;
 	};
 };
 
 export default async function MDXContentPage({ params: { mdxPageSlug: slug } }: PageParamsType) {
-	const { default: MDXContent, title } = await getComponentFromSlug(slug);
+	const MDXContent = dynamic(() => import(`./${slug}.mdx`));
 
-	return (
-		<Suspense fallback={<p>Loading...</p>}>
-			<h1 className="font-heading py-10 font-serif text-8xl">/{title.toLowerCase()}</h1>
-			<MDXContent />
-		</Suspense>
-	);
+	return <MDXContent />;
 }
