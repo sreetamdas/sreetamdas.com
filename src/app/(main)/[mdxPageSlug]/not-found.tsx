538b9aef6767687a26f92ed0d5944656ea10e7ac
Sreetam Das <sreetamdas@gmail.com>
2023-07-19 13:01:55 +0530

    Add mdxSlugPage files

--- src/app/(main)/[mdxPageSlug]/not-found.tsx
+++ src/app/(main)/[mdxPageSlug]/not-found.tsx
@@ -1,8 +1,3 @@
-export default function NewsletterEmailNotFound() {
-	return (
-		<>
-			<h1>Not Found</h1>
-			<p>The newsletter issue/email you&apos;re trying to find doesn&apos;t exist :/</p>
-		</>
-	);
+export default function MDXPageNotFound() {
+	return <h1>MDX page not Found</h1>;
 }
--- src/app/(main)/[mdxPageSlug]/page.tsx
+++ src/app/(main)/[mdxPageSlug]/page.tsx
@@ -1,32 +1,37 @@
-"use client";
+import { notFound } from "next/navigation";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { Suspense } from "react";
 
-import dynamic from "next/dynamic";
-// import { Suspense } from "react";
+import { allPages } from "contentlayer/generated";
 
-// export const dynamicParams = false;
+export const dynamicParams = false;
 
-// export async function generateStaticParams() {
-// 	const MDX_PAGES_DIR = path.resolve(process.cwd(), "src", "app", "(main)", "[mdxPageSlug]");
-// 	const files = (await fs.readdir(MDX_PAGES_DIR)).filter((file) => file.endsWith(".mdx"));
+export async function generateStaticParams() {
+	return allPages.map((post) => ({
+		slug: post.page_slug,
+	}));
+}
 
-// 	const params = files.map((fileName) => {
-// 		const pageName = fileName.replace(/\.mdx?$/, "");
-// 		return {
-// 			mdxPageSlug: pageName,
-// 		};
-// 	});
-
-// 	return params;
-// }
-
-type PageParamsType = {
+type PageParams = {
 	params: {
 		mdxPageSlug: string;
 	};
 };
+export default function MDXPageSlugPage({ params }: PageParams) {
+	console.log({ allPages: allPages.map((page) => page.page_slug), params });
 
-export default async function MDXContentPage({ params: { mdxPageSlug: slug } }: PageParamsType) {
-	const MDXContent = dynamic(() => import(`./${slug}.mdx`));
+	const post = allPages.find((page) => page.page_slug === params.mdxPageSlug);
 
-	return <MDXContent />;
+	if (!post) notFound();
+
+	const MDXContent = useMDXComponent(post.body.code);
+
+	return (
+		<>
+			<p>mdx slug: {params.mdxPageSlug}</p>
+			<Suspense>
+				<MDXContent />
+			</Suspense>
+		</>
+	);
 }
