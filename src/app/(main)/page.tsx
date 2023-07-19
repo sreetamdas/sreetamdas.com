ef87979b0635993cd8a3daa425b38c2939898b34
Sreetam Das <sreetamdas@gmail.com>
2023-07-19 13:01:10 +0530

    Add introduction MDX to homepage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,20 @@
+import { isUndefined } from "lodash-es";
+import { useMDXComponent } from "next-contentlayer/hooks";
 import { Suspense } from "react";
 
 import { ViewsCounter } from "@/lib/components/ViewsCounter";
+import { customMDXComponents } from "@/lib/domains/mdx";
+import { allPages } from "contentlayer/generated";
 
 export default function Home() {
+	const post = allPages.find((page) => page.page_slug === "introduction");
+
+	if (isUndefined(post)) {
+		throw new Error("introduction.mdx is missing");
+	}
+
+	const MDXContent = useMDXComponent(post.body.code);
+
 	return (
 		<>
 			<h1 className="py-20 text-center font-serif text-6xl">
@@ -12,6 +24,7 @@ export default function Home() {
 				</span>
 			</h1>
 			<Suspense>
+				<MDXContent components={customMDXComponents} />
 				<ViewsCounter slug="/" hidden />
 			</Suspense>
 		</>
