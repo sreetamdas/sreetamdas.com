5df3f5feb5c6b179340d93456fa0e98b3663166d
Sreetam Das <sreetamdas@gmail.com>
2023-07-29 21:30:05 +0530

    edit copy for blog not-found page

--- src/app/(main)/blog/[slug]/not-found.tsx
+++ src/app/(main)/blog/[slug]/not-found.tsx
@@ -2,7 +2,7 @@ export default function NewsletterEmailNotFound() {
 	return (
 		<>
 			<h1>Not Found</h1>
-			<p>The newsletter issue/email you&apos;re trying to find doesn&apos;t exist :/</p>
+			<p>The blog post you&apos;re trying to find doesn&apos;t exist :/</p>
 		</>
 	);
 }
