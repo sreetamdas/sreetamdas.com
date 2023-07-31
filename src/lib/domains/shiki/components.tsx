8685f1da2614afe4aa59201735dff12350f60780
Sreetam Das <sreetamdas@gmail.com>
2023-07-31 15:58:43 +0530

    add line highlight styles

--- src/lib/domains/shiki/components.tsx
+++ src/lib/domains/shiki/components.tsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx";
 import { isNull, isObject, isUndefined } from "lodash-es";
 import { type DetailedHTMLProps, type HTMLAttributes, type ReactNode } from "react";
 
@@ -28,7 +29,10 @@ export const CodeBlock = (props: CodeBlockProps) => {
 	}
 
 	return (
-		<pre className="my-5 -ml-12 -mr-5 overflow-x-scroll rounded-global p-5 text-sm" style={style}>
+		<pre
+			className="my-5 -ml-12 -mr-5 overflow-x-scroll rounded-global p-5 text-sm max-md:-ml-6"
+			style={style}
+		>
 			<span className="float-right -mt-5 rounded-b-global bg-zinc-800 px-1 py-2 text-zinc-400">
 				{language.toLocaleUpperCase()}
 			</span>
@@ -37,8 +41,19 @@ export const CodeBlock = (props: CodeBlockProps) => {
 					{children
 						?.filter((line) => line !== "\n")
 						.map((line, i) => (
-							<span key={i} data-highlight={shouldHighlightLine(i)} className="block">
-								<span className="-ml-2 mr-2 hidden w-[2rem] select-none pr-2 text-right text-zinc-600 md:inline-block">
+							<span
+								key={i}
+								className={clsx(
+									"block",
+									shouldHighlightLine(i) && "-mx-5 border-l-4 border-[#a86efd] bg-[#FFFFFF20] px-4"
+								)}
+							>
+								<span
+									className={clsx(
+										"-ml-2 mr-2 hidden w-[2rem] select-none pr-2 text-right text-zinc-600 md:inline-block",
+										shouldHighlightLine(i) && "text-[#a86efd]"
+									)}
+								>
 									{i + 1}
 								</span>
 								{line}
