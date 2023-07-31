ef131d44ff8248ab6ef28c22caede43216ee8a0f
Sreetam Das <sreetamdas@gmail.com>
2023-07-31 15:21:03 +0530

    fix InfoBlock `background-color` in dark mode

--- src/lib/components/MDX/utilities.tsx
+++ src/lib/components/MDX/utilities.tsx
@@ -1,5 +1,5 @@
 import { clsx } from "clsx";
-import type { ReactNode } from "react";
+import { type ReactNode } from "react";
 
 export const Gradient = ({ children }: { children: ReactNode }) => (
 	<span className="w-fit bg-gradient-to-r from-primary to-secondary box-decoration-slice bg-clip-text text-transparent">
@@ -17,7 +17,7 @@ export const InfoBlock = (props: InfoBlockProps) => {
 		<aside
 			className={clsx(
 				"my-5 -ml-12 -mr-5 rounded-global py-5 pl-12 pr-5",
-				type === "info" ? "border-l-4 border-l-indigo-500 bg-indigo-100" : ""
+				type === "info" ? "border-l-4 border-l-indigo-500 bg-indigo-100 dark:bg-indigo-950" : ""
 			)}
 		>
 			{children}
