baba368c18923ae6aa3a49877763a8cb83e7c04d
Sreetam Das <sreetamdas@gmail.com>
2023-08-03 14:45:59 +0530

    use `url` frontmatter for page_path when available
    allows for custom URLs and handling nested MDX pages

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -9,26 +9,28 @@ export default async function BlogArchivePage() {
 		<>
 			<h1 className="pb-20 pt-10 font-serif text-8xl">/blog</h1>
 			<section className="grid gap-20">
-				{blog_posts_previews.map(({ page_slug, page_path, title, description, published_at }) => (
-					<article key={page_slug} className="group grid gap-y-2">
-						<LinkTo href={page_path} className="w-fit hover:no-underline">
-							<h3 className="group-hover:gradient w-fit font-serif text-5xl leading-normal">
-								{title}
-							</h3>
-						</LinkTo>
-						<p className="">{description}</p>
+				{blog_posts_previews.map(
+					({ title, description, page_slug, page_path, published_at, url }) => (
+						<article key={page_slug} className="group grid gap-y-2">
+							<LinkTo href={url ?? page_path} className="w-fit hover:no-underline">
+								<h3 className="group-hover:gradient w-fit font-serif text-5xl leading-normal">
+									{title}
+								</h3>
+							</LinkTo>
+							<p className="">{description}</p>
 
-						<div className="flex justify-start gap-x-6">
-							<p className="text-sm">
-								{new Date(published_at).toLocaleDateString("en-US", {
-									year: "numeric",
-									month: "long",
-									day: "numeric",
-								})}
-							</p>
-						</div>
-					</article>
-				))}
+							<div className="flex justify-start gap-x-6">
+								<p className="text-sm">
+									{new Date(published_at).toLocaleDateString("en-US", {
+										year: "numeric",
+										month: "long",
+										day: "numeric",
+									})}
+								</p>
+							</div>
+						</article>
+					)
+				)}
 			</section>
 		</>
 	);
@@ -36,7 +38,7 @@ export default async function BlogArchivePage() {
 
 async function getAllBlogPostsPreviewsData() {
 	return allBlogPosts
-		.flatMap(({ title, description, page_slug, page_path, published, published_at }) => {
+		.flatMap(({ title, description, page_slug, page_path, url, published, published_at }) => {
 			if (!IS_DEV && !published) {
 				return [];
 			}
@@ -47,6 +49,7 @@ async function getAllBlogPostsPreviewsData() {
 				page_slug,
 				page_path,
 				published_at,
+				url,
 			};
 		})
 		.sort((a, b) => new Date(b.published_at).getTime() - new Date(a.published_at).getTime());
