77584b726e6497b6900d2d4f64842c011f51a52e
Sreetam Das <sreetamdas@gmail.com>
2023-08-03 23:44:47 +0530

    add missing style to make parent div full-height

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ import { Header } from "@/lib/components/Header";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
 	return (
-		<div className="grid grid-flow-col grid-cols-[1fr_min(var(--max-width),_calc(100%_-_2rem))_1fr] grid-rows-[auto_1fr_max-content] gap-x-5">
+		<div className="grid min-h-screen grid-flow-col grid-cols-[1fr_min(var(--max-width),_calc(100%_-_2rem))_1fr] grid-rows-[auto_1fr_max-content] gap-x-5">
 			<Header />
 			<main id="main-content" className="relative col-start-2 col-end-3 flex flex-col">
 				{children}
