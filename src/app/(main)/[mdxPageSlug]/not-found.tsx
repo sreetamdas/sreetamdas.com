8e20563609df3bcdf408af7eb32d9a7dc18bc2de
Sreetam Das <sreetamdas@gmail.com>
2023-08-04 09:19:46 +0530

    update MDXSlugPage not-found message

--- src/app/(main)/[mdxPageSlug]/not-found.tsx
+++ src/app/(main)/[mdxPageSlug]/not-found.tsx
@@ -1,3 +1,8 @@
-export default function MDXPageNotFound() {
-	return <h1>MDX page not Found</h1>;
+export default function MDXSlugPageNotFound() {
+	return (
+		<>
+			<h1>Not Found</h1>
+			<p>The MDX post you&apos;re trying to find doesn&apos;t exist :/</p>
+		</>
+	);
 }
