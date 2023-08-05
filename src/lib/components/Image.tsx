43da0bbf6f19a57a2370c98f2961bcb99d858222
Sreetam Das <sreetamdas@gmail.com>
2023-08-05 16:08:19 +0530

    update `Image` for closer and custom props

--- src/lib/components/Image.tsx
+++ src/lib/components/Image.tsx
@@ -1,24 +1,42 @@
+import { isObject } from "lodash-es";
+import { type PlaceholderValue, type StaticImport } from "next/dist/shared/lib/get-img-props";
 import NextImage from "next/image";
-import { type DetailedHTMLProps, type ImgHTMLAttributes } from "react";
-
-// export const ImageWrapper = styled.span`
-// 	& img {
-// 		max-width: var(--max-width);
-// 		width: 100%;
-// 		height: auto;
-// 		border-radius: var(--border-radius);
-// 	}
-// `;
+import { type DetailedHTMLProps, type ImgHTMLAttributes, type RefAttributes } from "react";
 
 type SafeNumber = number | `${number}`;
-type ImageProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
-
+type CustomImageProps = {
+	isWrapped?: boolean;
+};
+type ImageProps = Omit<
+	DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>,
+	"height" | "width" | "loading" | "ref" | "alt" | "src" | "srcSet"
+> & {
+	src: string | StaticImport;
+	alt: string;
+	width?: SafeNumber;
+	height?: SafeNumber;
+	quality?: SafeNumber;
+	priority?: boolean;
+	loading?: "eager" | "lazy";
+	placeholder?: PlaceholderValue;
+	unoptimized?: boolean;
+} & RefAttributes<HTMLImageElement | null> &
+	CustomImageProps;
 export const Image = (props: ImageProps) => {
-	const { alt = " ", src, height, width } = props;
+	const { alt = " ", src, height, width, isWrapped = false } = props;
 
 	if (typeof src === "undefined") return null;
-	const type = src.slice(-3);
+	if ((typeof height !== "undefined" && typeof width !== "undefined") || isObject(src)) {
+		return isWrapped ? (
+			<NextImage alt={alt} src={src} height={height} width={width} quality="100" />
+		) : (
+			<span className="[&_img]:h-auto [&_img]:w-full [&_img]:max-w-[--max-width] [&_img]:rounded-global">
+				<NextImage alt={alt} src={src} height={height} width={width} quality="100" />
+			</span>
+		);
+	}
 
+	const type = src.slice(-3);
 	if (type === "mp4") {
 		return (
 			<video
@@ -37,23 +55,13 @@ export const Image = (props: ImageProps) => {
 			</video>
 		);
 	}
-	if (typeof height !== "undefined" && typeof width !== "undefined") {
-		return (
-			<span>
-				<NextImage
-					alt={alt}
-					src={src}
-					height={height as SafeNumber}
-					width={width as SafeNumber}
-					quality="100"
-				/>
-			</span>
-		);
-	}
 
 	// for external images
-	return (
-		<span>
+	return isWrapped ? (
+		// eslint-disable-next-line @next/next/no-img-element
+		<img src={src} alt={alt} loading="lazy" />
+	) : (
+		<span className="[&_img]:h-auto [&_img]:w-full [&_img]:max-w-[--max-width] [&_img]:rounded-global">
 			{/* eslint-disable-next-line @next/next/no-img-element */}
 			<img src={src} alt={alt} loading="lazy" />
 		</span>
