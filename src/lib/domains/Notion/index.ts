91fbceb15bfddb4bf7681a09d5f03e186f68bf11
Sreetam Das <sreetamdas@gmail.com>
2023-08-05 11:26:24 +0530

    fix Notion query

--- src/lib/domains/Notion/index.ts
+++ src/lib/domains/Notion/index.ts
@@ -63,7 +63,7 @@ export class NotionClient {
 			const property_ids = await this.getPropertiesIDs(database_id, filter_properties);
 
 			filter_properties_query =
-				"?" + property_ids?.map(({ id }) => `filter_properties=[${id}]`).join("&");
+				"?" + property_ids?.map(({ id }) => `filter_properties=${id}`).join("&");
 		}
 
 		const query_string = `https://api.notion.com/v1/databases/${database_id}/query${filter_properties_query}`;
