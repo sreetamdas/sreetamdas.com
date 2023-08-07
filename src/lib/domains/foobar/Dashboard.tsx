1e86663b833f77d07583b12b9daaba6f5ec65369
Sreetam Das <sreetamdas@gmail.com>
2023-08-07 21:39:45 +0530

    add alert dialog for /foobar reset, upgrade deps
    add @radix-ui/react-{toast,alert-dialog}

--- src/lib/domains/foobar/Dashboard.tsx
+++ src/lib/domains/foobar/Dashboard.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 import GlobalNotFound from "@/app/not-found";
 import { IS_DEV } from "@/config";
+import { Code } from "@/lib/components/Typography";
 import { useCustomPlausible } from "@/lib/domains/Plausible";
 import { ShowCompletedBadges } from "@/lib/domains/foobar/badges";
 import {
@@ -56,12 +58,7 @@ export const FoobarDashboard = ({ completedPage }: FoobarSchrodingerProps) => {
 				completed={foobarData.completed}
 				allAchievements={foobarData.allAchievements}
 			/>
-			<button
-				className="rounded-global border-2 border-solid border-primary bg-background px-6 py-1 text-sm text-foreground transition-[color,background-color] hover:bg-primary hover:text-background"
-				onClick={handleClearFoobarData}
-			>
-				Clear everything and Restart
-			</button>
+			<ResetFoobar handleClearFoobarData={handleClearFoobarData} />
 			{/* <Center>
 				<SupportSreetamDas />
 			</Center> */}
@@ -96,6 +93,44 @@ const UnlockedAchievementBanner = ({ completedPage }: FoobarSchrodingerProps) =>
 	) : null;
 
 const XMarksTheSpot = (_props: { foobar: string }) => null;
+
+const ResetFoobar = ({ handleClearFoobarData }: { handleClearFoobarData: () => void }) => (
+	<AlertDialogPrimitive.Root>
+		<AlertDialogPrimitive.Trigger asChild>
+			<button className="rounded-global border-2 border-solid border-primary bg-background px-6 py-1 text-sm text-foreground transition-[color,background-color] hover:bg-primary hover:text-background">
+				Clear everything and Restart
+			</button>
+		</AlertDialogPrimitive.Trigger>
+		<AlertDialogPrimitive.Portal>
+			<AlertDialogPrimitive.Overlay className="fixed inset-0 bg-slate-950/40 data-[state=open]:animate-overlayShow" />
+			<AlertDialogPrimitive.Content className="fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-global bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
+				<AlertDialogPrimitive.Title className="m-0 text-[17px] font-medium text-slate-950">
+					Are you absolutely sure?
+				</AlertDialogPrimitive.Title>
+				<AlertDialogPrimitive.Description className="mb-5 mt-4 text-[15px] leading-normal text-zinc-500">
+					This action cannot be undone.
+					<br />
+					This will reset your <Code>/foobar</Code> progress.
+				</AlertDialogPrimitive.Description>
+				<div className="flex justify-end gap-[25px]">
+					<AlertDialogPrimitive.Cancel asChild>
+						<button className="inline-flex h-[35px] items-center justify-center rounded-global bg-zinc-100 px-[15px] font-medium leading-none text-zinc-500 outline-none hover:bg-zinc-200 focus:shadow-[0_0_0_2px] focus:shadow-zinc-300">
+							Cancel
+						</button>
+					</AlertDialogPrimitive.Cancel>
+					<AlertDialogPrimitive.Action asChild>
+						<button
+							className="inline-flex h-[35px] items-center justify-center rounded-global bg-red-100 px-[15px] font-medium leading-none text-red-700 outline-none hover:bg-red-200 focus:shadow-[0_0_0_2px] focus:shadow-red-300"
+							onClick={handleClearFoobarData}
+						>
+							Yes, delete account
+						</button>
+					</AlertDialogPrimitive.Action>
+				</div>
+			</AlertDialogPrimitive.Content>
+		</AlertDialogPrimitive.Portal>
+	</AlertDialogPrimitive.Root>
+);
 
 const FoobarButLocked = () => (
 	<GlobalNotFound
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,18 @@ export default {
 					"0%, 100%": { transform: "scale(0)" },
 					"50%": { transform: "scale(1)" },
 				},
+				overlayShow: {
+					from: { opacity: "0" },
+					to: { opacity: "1" },
+				},
+				contentShow: {
+					from: { opacity: "0", transform: "translate(-50%, -48%) scale(0.96)" },
+					to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+				},
+			},
+			animation: {
+				overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+				contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
 			},
 		},
 	},
