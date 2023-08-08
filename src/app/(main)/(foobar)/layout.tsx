2cbe7b6243cd9475d8084814f3b3bccfbfe1d876
Sreetam Das <sreetamdas@gmail.com>
2023-08-08 12:35:41 +0530

    use metadata API, remove `<meta>` tags

--- src/app/(main)/(foobar)/layout.tsx
+++ src/app/(main)/(foobar)/layout.tsx
@@ -3,3 +3,9 @@ import { type ReactNode } from "react";
 export default function FoobarLayout({ children }: { children: ReactNode }) {
 	return children;
 }
+
+export const metadata = {
+	robots: {
+		index: false,
+	},
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import PlausibleProvider from "next-plausible";
-
 import "./global.css";
 
-import { DocumentHead } from "@/lib/domains/SEO";
+import PlausibleProvider from "next-plausible";
+
+import { SITE_URL } from "@/config";
 import { blockingScriptSetInitialColorScheme } from "@/lib/domains/colorScheme/blockingScript";
 import { inter_font, iosevka_font, eb_garamond_font } from "@/lib/styles/fonts";
 
@@ -16,10 +16,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		>
 			<head>
 				<link rel="icon" href="/favicon.png" />
-				<meta name="apple-mobile-web-app-title" content="Sreetam Das' Blog" />
-				<meta name="apple-mobile-web-app-status-bar-style" content="default" />
-				<meta name="apple-mobile-web-app-capable" content="yes" />
-				<meta name="mobile-web-app-capable" content="yes" />
 
 				<PlausibleProvider
 					domain="sreetamdas.com"
@@ -27,7 +23,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					trackOutboundLinks
 					trackFileDownloads
 				/>
-				<DocumentHead title="Hello hello!" />
 			</head>
 			<body>
 				<script
@@ -40,3 +35,39 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		</html>
 	);
 }
+
+export const metadata = {
+	title: "Hello hello! | Sreetam Das",
+	description:
+		"Senior software tinkerer from India. 💜 React, Elixir and TypeScript, CS:GO and mechanical keyboards!",
+	openGraph: {
+		title: "👋 Hello! | Sreetam Das",
+		description:
+			"Senior software tinkerer from India. 💜 React, Elixir and TypeScript, CS:GO and mechanical keyboards!",
+		url: SITE_URL,
+		type: "website",
+	},
+	twitter: {
+		title: "👋 Hello! | Sreetam Das",
+		description:
+			"Senior software tinkerer from India. 💜 React, Elixir and TypeScript, CS:GO and mechanical keyboards!",
+		card: "summary_large_image",
+		creator: "@_SreetamDas",
+		site: "@_SreetamDas",
+	},
+	appleWebApp: {
+		title: "Sreetam Das' website",
+		statusBarStyle: "default",
+		capable: true,
+	},
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "#5B34DA" },
+		{ media: "(prefers-color-scheme: dark)", color: "#9D86E9" },
+	],
+	icons: {
+		icon: "/favicon.png",
+	},
+	other: {
+		"mobile-web-app-capable": "yes",
+	},
+};
