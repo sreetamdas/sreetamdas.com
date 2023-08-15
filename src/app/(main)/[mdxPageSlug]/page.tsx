44836dd6c6be78bdd166a5139a3e7e0d0289cece
Sreetam Das <sreetamdas@gmail.com>
2023-08-15 21:01:15 +0530

    add `metadata` for `/[mdxPageSlug]`

--- src/app/(main)/[mdxPageSlug]/page.tsx
+++ src/app/(main)/[mdxPageSlug]/page.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from "next";
 import { notFound } from "next/navigation";
 
+import { SITE_TITLE_APPEND, SITE_URL, SITE_OG_IMAGE } from "@/config";
 import { MDXContent } from "@/lib/components/MDX";
 import { ViewsCounter } from "@/lib/components/ViewsCounter";
 import { RepoContributors } from "@/lib/domains/GitHub";
@@ -37,12 +38,24 @@ export async function generateStaticParams() {
 	});
 }
 
-export async function generateMetadata({ params: { mdxPageSlug } }: PageParams): Promise<Metadata> {
-	const post = allPages.find((page) => page.page_slug === mdxPageSlug);
+export async function generateMetadata({ params }: PageParams): Promise<Metadata> {
+	const post = allPages.find((page) => page.page_slug === params.mdxPageSlug);
 
 	return {
 		title: post?.seo_title ?? post?.title,
 		description: post?.description,
-		// TODO add image
+		openGraph: {
+			title: `${post?.seo_title ?? post?.title} ${SITE_TITLE_APPEND}`,
+			description: post?.description,
+			type: "article",
+			url: `${SITE_URL}/${params.mdxPageSlug}`,
+			images: { url: SITE_OG_IMAGE },
+		},
+		twitter: {
+			card: "summary_large_image",
+			title: `${post?.seo_title ?? post?.title} ${SITE_TITLE_APPEND}`,
+			description: post?.description,
+			images: { url: SITE_OG_IMAGE },
+		},
 	};
 }
