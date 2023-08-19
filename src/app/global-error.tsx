74a0bbe49b76d7d3c6f0369a2ceb2b5399cdf838
Sreetam Das <sreetamdas@gmail.com>
2023-08-19 17:43:32 +0530

    add global style classes to `{global-error,not-found].tsx`

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,13 +1,37 @@
 "use client";
 
+import PlausibleProvider from "next-plausible";
+
+import { blockingScriptSetInitialColorScheme } from "@/lib/domains/colorScheme/blockingScript";
+import { inter_font, iosevka_font, eb_garamond_font } from "@/lib/domains/fonts";
+
 export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
 	// eslint-disable-next-line no-console
 	console.error("Caught ==>", error);
 
 	return (
-		<html lang="en">
-			<head></head>
-			<body>
+		<html
+			lang="en"
+			dir="ltr"
+			className={`scroll-pt-16 scroll-smooth ${inter_font.variable} ${iosevka_font.variable} ${eb_garamond_font.variable}`}
+			suppressHydrationWarning
+		>
+			<head>
+				<link rel="icon" href="/favicon.png" />
+
+				<PlausibleProvider
+					domain="sreetamdas.com"
+					customDomain="sreetamdas.com"
+					trackOutboundLinks
+					trackFileDownloads
+				/>
+			</head>
+			<body className="min-h-screen bg-background text-foreground selection:bg-secondary selection:text-background">
+				<script
+					dangerouslySetInnerHTML={{
+						__html: blockingScriptSetInitialColorScheme,
+					}}
+				></script>
 				<h2>Something went wrong!</h2>
 				<button onClick={() => reset()}>Try again</button>
 			</body>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,8 @@ import { type ReactNode } from "react";
 
 import { LinkTo } from "@/lib/components/Anchor";
 import { NotFoundDogsLink, NotFoundFoobarTracker } from "@/lib/components/Error";
+import { Footer } from "@/lib/components/Footer";
+import { Header } from "@/lib/components/Header";
 
 export type GlobalNotFoundPageProps = {
 	message?: ReactNode;
@@ -9,19 +11,26 @@ export type GlobalNotFoundPageProps = {
 export default function GlobalNotFound({ message }: GlobalNotFoundPageProps) {
 	return (
 		<>
-			<h1 className="pt-10 text-center font-serif text-[160px]">404!</h1>
-			<p className="pt-4 text-center font-serif text-3xl">
-				The page you&apos;re looking for does not exist.
-			</p>
+			<Header />
+			<main
+				id="main-content"
+				className="relative grid grid-flow-col grid-cols-[1fr_min(var(--max-width),_calc(100%_-_2rem))_1fr] gap-x-5 children:[grid-column:2]"
+			>
+				<h1 className="pt-10 text-center font-serif text-[160px]">404!</h1>
+				<p className="pt-4 text-center font-serif text-3xl">
+					The page you&apos;re looking for does not exist.
+				</p>
 
-			{message ? message : null}
+				{message ? message : null}
 
-			<p className="pt-40 text-center">
-				<LinkTo href="/">Go back home</LinkTo>
-			</p>
+				<p className="pt-40 text-center">
+					<LinkTo href="/">Go back home</LinkTo>
+				</p>
 
-			<NotFoundDogsLink />
-			<NotFoundFoobarTracker />
+				<NotFoundDogsLink />
+				<NotFoundFoobarTracker />
+			</main>
+			<Footer />
 		</>
 	);
 }
