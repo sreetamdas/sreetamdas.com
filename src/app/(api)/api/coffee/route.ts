9aa094aa83c0ce2614c93fc2723a14835b21f0d7
Sreetam Das <sreetamdas@gmail.com>
2023-08-20 21:44:34 +0530

    improve `/api/coffee` routeh handler

--- src/app/(api)/api/coffee/route.ts
+++ src/app/(api)/api/coffee/route.ts
@@ -3,9 +3,12 @@ import { NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function POST() {
-	return NextResponse.json({ message: "I'm a teapot!", foobar: "/foobar/teapot" }, { status: 418 });
+	return NextResponse.json(
+		{ message: "Cannot brew coffeee, I'm a teapot", foobar: "/foobar/teapot" },
+		{ status: 418 },
+	);
 }
 
 export async function GET() {
-	return NextResponse.json({ message: "POST a request, please ;)" }, { status: 303 });
+	return NextResponse.json({}, { status: 405, headers: { Allow: "POST" } });
 }
