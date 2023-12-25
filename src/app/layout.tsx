eae87fb29627570e0f2c1a8f9372278f3219c3b2
Sreetam Das <sreetamdas@gmail.com>
2023-12-25 19:03:57 +0530

    inline again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./global.css";
 import PlausibleProvider from "next-plausible";
 
 import { SITE_DESCRIPTION, SITE_TITLE_APPEND, SITE_URL } from "@/config";
-import { blockingScriptSetInitialColorScheme } from "@/lib/domains/colorScheme/blockingScript";
 import { inter_font, iosevka_font, eb_garamond_font } from "@/lib/domains/fonts";
 import { FOOBAR_SOURCE_CODE } from "@/lib/domains/foobar/helpers";
 
@@ -78,3 +77,51 @@ export const viewport = {
 		{ media: "(prefers-color-scheme: dark)", color: "#9D86E9" },
 	],
 };
+
+function setInitialColorScheme() {
+	function getInitialColorScheme() {
+		const persistedColorScheme = window.localStorage.getItem("color-scheme");
+		const hasPersistedColorScheme = typeof persistedColorScheme === "string";
+
+		/**
+		 * If the user has explicitly chosen light or dark, use it
+		 */
+		if (hasPersistedColorScheme) {
+			const root = window.document.documentElement;
+			root.style.setProperty("--initial-color-scheme", persistedColorScheme);
+
+			if (persistedColorScheme !== "system") {
+				return persistedColorScheme;
+			}
+		}
+
+		/**
+		 * If they haven't been explicit, check the media query
+		 */
+		const mql = window.matchMedia("(prefers-color-scheme: dark)");
+		const hasSystemColorSchemePreference = typeof mql.matches === "boolean";
+
+		if (hasSystemColorSchemePreference) {
+			return mql.matches ? "dark" : "light";
+		}
+
+		/**
+		 * If they are using a browser/OS that doesn't support
+		 * color themes, default to 'light'.
+		 */
+		return "light";
+	}
+
+	const colorScheme = getInitialColorScheme();
+	if (colorScheme === "dark") {
+		document.documentElement.setAttribute("data-color-scheme", "dark");
+	}
+}
+
+const blockingScriptSetInitialColorScheme = `(function() {
+	${setInitialColorScheme.toString()}
+	setInitialColorScheme();
+})()
+
+// IIFE!
+`;
