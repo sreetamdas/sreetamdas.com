592ed05dc2197194aaf2d39bf38c232b7587f018
Sreetam Das <sreetamdas@gmail.com>
2024-04-03 22:01:29 -0500

    Try WASM approach

--- src/lib/domains/shiki/highlighter.ts
+++ src/lib/domains/shiki/highlighter.ts
@@ -6,16 +6,10 @@ import {
 	getHighlighter,
 	normalizeTheme,
 } from "shiki/bundle-web.mjs";
-import { getHighlighterCore } from "shiki/core";
+import { getHighlighterCore, loadWasm } from "shiki/core";
 import elixirLang from "shiki/langs/elixir.mjs";
-// import JavaScriptLang from "shiki/langs/javascript.mjs";
-// import JSXLang from "shiki/langs/jsx.mjs";
 import markdownLang from "shiki/langs/markdown.mjs";
 import typeScriptLang from "shiki/langs/typescript.mjs";
-import getWasm from "shiki/wasm";
-// import TSXLang from "shiki/langs/tsx.mjs";
-// import JSONLang from "shiki/langs/json.mjs";
-// import MDXLang from "shiki/langs/mdx.mjs"
 
 export type BundledLangs = (typeof preloaded_langs)[number] | "elixir";
 export const preloaded_langs = [
@@ -65,10 +59,13 @@ async function getPureKarmaHighlighter(): Promise<KarmaHighlighter> {
 	const karma_shiki_theme = convertToThemeRegistration(defaultTheme);
 	const theme = normalizeTheme(karma_shiki_theme);
 
+	// @ts-expect-error WASM
+	await loadWasm(import("shiki/onig.wasm"));
+
 	const highlighter = (await getHighlighterCore({
 		langs: [elixirLang, typeScriptLang, markdownLang],
 		themes: [theme],
-		loadWasm: getWasm,
+		// loadWasm: getWasm,
 	})) as unknown as KarmaHighlighter;
 
 	return highlighter;
