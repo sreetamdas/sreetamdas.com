2b22d63695be4397c5186e4cf2712dc931cfa9c7
Sreetam Das <sreetamdas@gmail.com>
2024-07-28 23:54:34 +0530

    Revert "use precompiled shiki move"
    This reverts commit c75e3887391c46fb8021f29f6f4b1b9d4b47ab4f.

--- src/app/(main)/aoc/[...slug]/pulse-propagation/index.tsx
+++ src/app/(main)/aoc/[...slug]/pulse-propagation/index.tsx
@@ -3,14 +3,12 @@ import "shiki-magic-move/dist/style.css";
 
 import { useEffect, useState } from "react";
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
-import { ShikiMagicMovePrecompiled } from "shiki-magic-move/react";
-import { type KeyedTokensInfo } from "shiki-magic-move/types";
+import { ShikiMagicMove } from "shiki-magic-move/react";
 
 import { getSlimKarmaHighlighter } from "@/lib/domains/shiki";
 import { type KarmaHighlighter } from "@/lib/domains/shiki/highlighter";
 
-type Props = { compiledSteps: Array<KeyedTokensInfo> };
-export const ParseInput = ({ compiledSteps }: Props) => {
+export const ParseInput = () => {
 	const [currentStageIndex, setCurrentStageIndex] = useState(0);
 	const [highlighter, setHighlighter] = useState<KarmaHighlighter>();
 
@@ -67,9 +65,14 @@ export const ParseInput = ({ compiledSteps }: Props) => {
 					</span>
 				</div>
 				{highlighter && (
-					<div className="-ml-12 -mr-5 overflow-x-scroll rounded-bl-global rounded-br-global bg-karma-background p-5 text-xs max-sm:-ml-4 max-sm:px-2 sm:text-sm [&_.shiki-magic-move-line-number]:mr-2 [&_.shiki-magic-move-line-number]:w-[1.5rem] [&_.shiki-magic-move-line-number]:whitespace-nowrap [&_.shiki-magic-move-line-number]:pr-2 [&_.shiki-magic-move-line-number]:text-right max-sm:[&_.shiki-magic-move-line-number]:hidden">
-						<ShikiMagicMovePrecompiled steps={compiledSteps} step={currentStageIndex} />
-					</div>
+					<ShikiMagicMove
+						lang="elixir"
+						theme="karma"
+						highlighter={highlighter}
+						code={stages[currentStageIndex].code.trim()}
+						options={{ duration: 300, stagger: 0, lineNumbers: true }}
+						className="-ml-12 -mr-5 overflow-x-scroll rounded-bl-global rounded-br-global p-5 text-xs max-sm:-ml-4 max-sm:px-2 sm:text-sm [&>.shiki-magic-move-line-number]:mr-2 [&>.shiki-magic-move-line-number]:w-[1.5rem] [&>.shiki-magic-move-line-number]:whitespace-nowrap [&>.shiki-magic-move-line-number]:pr-2 [&>.shiki-magic-move-line-number]:text-right max-sm:[&>.shiki-magic-move-line-number]:hidden"
+					/>
 				)}
 			</div>
 		</figure>
