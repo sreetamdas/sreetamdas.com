ced3e24853546566e83041c1125e3d79f46b57b4
Sreetam Das <sreetamdas@gmail.com>
2024-10-05 13:26:06 +0530

    fix: text spacing and weight on `/karma`

--- src/app/(main)/karma/page.tsx
+++ src/app/(main)/karma/page.tsx
@@ -49,8 +49,12 @@ export default function KarmaPage() {
 
 	return (
 		<>
-			<h1 className="pb-20 pt-10 text-center font-serif text-9xl leading-none">Karma</h1>
-			<p className="text-center font-serif text-4xl">A colorful VS Code theme</p>
+			<h1 className="pb-20 pt-10 text-center font-serif text-9xl font-bold leading-none tracking-tighter">
+				Karma
+			</h1>
+			<p className="text-center font-serif text-4xl font-medium tracking-tight">
+				A colorful VS Code theme
+			</p>
 
 			<KarmaShowcase examples={examples} />
 
