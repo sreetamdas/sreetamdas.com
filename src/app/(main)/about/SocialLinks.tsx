6f3562f6c6282808bbe4a3d79a4d3313b47a3c36
Sreetam Das <sreetamdas@gmail.com>
2025-02-09 18:25:41 -0500

    fix: typescript error for SocialLink

--- src/app/(main)/about/SocialLinks.tsx
+++ src/app/(main)/about/SocialLinks.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { cloneElement, type ReactElement } from "react";
+import { type ReactElement } from "react";
+import { IconBaseProps } from "react-icons";
 import {
 	FaDiscord,
 	FaEnvelope,
@@ -16,13 +17,10 @@ import { SiPeerlist } from "react-icons/si";
 
 import { LinkTo } from "@/lib/components/Anchor";
 
-const IconWithProps = ({ icon, title }: { icon: ReactElement; title: string }) =>
-	cloneElement(icon, { title });
-
 type ExternalLinksArrayType = Array<{
 	link: string;
 	title: string;
-	icon: ReactElement;
+	Icon: (props: IconBaseProps) => ReactElement;
 	onClick?: () => void;
 }>;
 export const SocialLinks = () => {
@@ -30,42 +28,42 @@ export const SocialLinks = () => {
 		{
 			link: "https://github.com/sreetamdas",
 			title: "Sreetam Das' GitHub",
-			icon: <FaGithub />,
+			Icon: (props) => <FaGithub {...props} />,
 		},
 		{
 			link: "https://twitter.com/_SreetamDas",
 			title: "Sreetam Das' Twitter",
-			icon: <FaTwitter />,
+			Icon: (props) => <FaTwitter {...props} />,
 		},
 		{
 			link: "https://stackoverflow.com/users/5283213",
 			title: "Sreetam Das' StackOverflow",
-			icon: <FaStackOverflow />,
+			Icon: (props) => <FaStackOverflow {...props} />,
 		},
 		{
 			link: "https://peerlist.io/sreetamdas",
 			title: "Sreetam Das' Peerlist",
-			icon: <SiPeerlist />,
+			Icon: (props) => <SiPeerlist {...props} />,
 		},
 		{
 			link: "https://www.linkedin.com/in/sreetamdas",
 			title: "Sreetam Das' LinkedIn",
-			icon: <FaLinkedin />,
+			Icon: (props) => <FaLinkedin {...props} />,
 		},
 		{
 			link: "mailto:sreetam@sreetamdas.com",
 			title: "Send email to Sreetam Das",
-			icon: <FaEnvelope />,
+			Icon: (props) => <FaEnvelope {...props} />,
 		},
 		{
 			link: "https://steamcommunity.com/id/karmanaut007",
 			title: "Sreetam Das' Steam",
-			icon: <FaSteam />,
+			Icon: (props) => <FaSteam {...props} />,
 		},
 		{
 			link: "https://giphy.com/gifs/LrmU6jXIjwziE/tile",
 			title: "Sreetam Das' Reddit",
-			icon: <FaRedditAlien />,
+			Icon: (props) => <FaRedditAlien {...props} />,
 			onClick: () => {
 				// TODO: add Plausible event to track this as a new foobar badge
 				// eslint-disable-next-line no-console
@@ -75,21 +73,21 @@ export const SocialLinks = () => {
 		{
 			link: "https://open.spotify.com/user/22nkuerb2tgjpqwhy4tp4aecq",
 			title: "Sreetam Das' Spotify",
-			icon: <FaSpotify />,
+			Icon: (props) => <FaSpotify {...props} />,
 		},
 		{
 			link: "https://srtm.fyi/ds",
 			title: "Join Sreetam Das' Discord server",
-			icon: <FaDiscord />,
+			Icon: (props) => <FaDiscord {...props} />,
 		},
 	];
 
 	return (
 		<ul className="flex flex-wrap place-items-center justify-center gap-4 px-12 py-4 sm:p-4">
-			{external_links.map(({ link, onClick, ...props }) => (
-				<li key={props.title} className="text-2xl">
+			{external_links.map(({ link, onClick, Icon, title }) => (
+				<li key={title} className="text-2xl">
 					<LinkTo href={link} onClick={onClick}>
-						<IconWithProps {...props} />
+						<Icon title={title} />
 					</LinkTo>
 				</li>
 			))}
