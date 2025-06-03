b57062a25954a1247468196b1dd488057f58f28c
Sreetam Das <sreetamdas@gmail.com>
2025-06-03 16:45:21 +0530

    chore: update resume headline

--- src/app/(pure)/resume/page.tsx
+++ src/app/(pure)/resume/page.tsx
@@ -15,7 +15,7 @@ export default function ResumePage() {
 							<h1 className="text-5xl font-bold leading-normal tracking-tight">Sreetam Das</h1>
 							<p className="leading-tight">
 								Open source aficionado who loves minimalist interfaces and component based,
-								deterministic development.
+								deterministic development — and wordy sentences.
 							</p>
 						</header>
 						<div className="flex flex-col items-end justify-end">
