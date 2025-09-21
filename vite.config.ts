f567c69c9f552c3c5fc864c2fcca596128fde22b
Sreetam Das <sreetamdas@gmail.com>
2025-09-21 12:34:38 +0530

    trigger

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
 	server: {
