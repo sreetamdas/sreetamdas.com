469b45557d2d5496a1c2aa24cecad4854f88d423
Sreetam Das <sreetamdas@gmail.com>
2026-05-11 00:06:57 +0530

    add fmt sort imports

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,17 @@ export default defineConfig({
 		printWidth: 100,
 		trailingComma: "all",
 		experimentalTailwindcss: {},
+		sortImports: {
+			groups: [
+				"type-import",
+				["value-builtin", "value-external"],
+				"type-internal",
+				"value-internal",
+				["type-parent", "type-sibling", "type-index"],
+				["value-parent", "value-sibling", "value-index"],
+				"unknown",
+			],
+		},
 		ignorePatterns: [
 			"node_modules",
 			"dist",
