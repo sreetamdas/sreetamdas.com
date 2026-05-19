ae516249b4af8de006da22ed77a98c8edc331de2
Sreetam Das <sreetamdas@gmail.com>
2026-05-19 12:31:43 +0530

    normalize api json responses

--- src/routes/(api)/api/coffee.ts
+++ src/routes/(api)/api/coffee.ts
@@ -4,11 +4,11 @@ export const Route = createFileRoute("/(api)/api/coffee")({
 	server: {
 		handlers: {
 			GET: () => {
-				return new Response(JSON.stringify({}), { status: 405, headers: { Allow: "POST" } });
+				return Response.json({}, { status: 405, headers: { Allow: "POST" } });
 			},
 			POST: () => {
-				return new Response(
-					JSON.stringify({ message: "Cannot brew coffee, I'm a teapot", foobar: "/foobar/teapot" }),
+				return Response.json(
+					{ message: "Cannot brew coffee, I'm a teapot", foobar: "/foobar/teapot" },
 					{ status: 418 },
 				);
 			},
